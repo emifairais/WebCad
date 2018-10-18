3c2ba375321c3833e34a9815e9931f18f888b273
emifairais <emifairais@yahoo.com>
2018-10-18 11:26:41 +0200

    Am instalat ESLint si toate cele. Am lucrat la unele dintre erorile semnalate.

--- src/Components/App.jsx
+++ src/Components/App.jsx
@@ -1,24 +1,29 @@
 import React, { Component } from 'react';
-import '../App.css'
-import Header from './Header.jsx';
-import Sidebar from './Sidebar.jsx';
-import Properties from './Properties.jsx'
-import MainComponent from './Main.jsx';
-import Drawer from './DrawingArea.jsx';
-
+import '../App.css';
+import Header from './Header';
+import Sidebar from './Sidebar';
+import Properties from './Properties';
+import MainComponent from './Main';
+import Drawer from './DrawingArea';
 
 
 class App extends Component {
-  constructor(props){
+  constructor(props) {
     super(props);
     this.state = {
-      selectedTool: "",
+      selectedTool: '',
       elements: [],
-      selectedElementId:"",
-      currentKeyArray:[],
-      indexSelectedObject: "",
-      selectedObject: {}
-    }
+      selectedElementId: '',
+      currentKeyArray: [],
+      indexSelectedObject: '',
+      selectedObject: {},
+    };
+    this.selectTool = this.selectTool.bind(this);
+    this.setSelectedId = this.setSelectedId.bind(this);
+    this.updateElements = this.updateElements.bind(this);
+    this.deleteElement = this.deleteElement.bind(this);
+    this.getValidationState = this.getValidationState.bind(this);
+    this.updateObjectInElements = this.updateObjectInElements.bind(this);
   }
 
 
@@ -26,99 +31,96 @@ class App extends Component {
     return elements.filter(element => element.id === selectedElementId)[0];
   }
 
-/*o sa fie onClick la properties*/
- updateObjectInElements(obj) {
-   const { indexSelectedObject, elements } = this.state;
-   const newElements = Object.assign([], elements);
-   newElements[indexSelectedObject] = obj;
-   this.setState({
-     elements: newElements
-   })
- }
 
-
- updateElements(obj) {
-   const { elements } = this.state;
-   const newElements = Object.assign([], elements);
-   newElements.push(obj);
-   this.setState({
-     elements: newElements
-   })
- }
-
-
-getValidationState() {
-    const length = 1
+  getValidationState() {
+    const length = 1;
     if (length > 0.5) return 'success';
-    else if (length > 0) return 'error';
+    if (length > 0) return 'error';
     return null;
   }
 
 
-
-
-
-
-
-selectTool(selectedTool) {
-      if(selectedTool === ""){
-        this.setState({
-          selectedTool: "",
-          selectedElementId: ""
-        })
-
-      } else {
-        if(this.state.selectedTool === selectedTool){
-          this.setState({
-            selectedTool:""
-          })
-        } else {
-        this.setState({
-          selectedTool,
-          selectedElementId: ""
-        })
-  }
-}
-}
-
-
-removeById(elements, id) {
-  const elementsUpdated = elements.filter(element => element.id !== id);
-  return elementsUpdated;
-}
-
-/*onClick pe drawGeometry din DrawingArea*/
-setSelectedId(element) {
+  /* onClick pe drawGeometry din DrawingArea */
+  setSelectedId(element) {
     const { selectedElementId, elements } = this.state;
-    if(selectedElementId === element.id){
+    if (selectedElementId === element.id) {
       this.setState({
-        selectedElementId: "",
+        selectedElementId: '',
         selectedObject: {},
-        indexSelectedObject: ""
-      })
+        indexSelectedObject: '',
+      });
     } else {
       this.setState({
-      selectedElementId: element.id,
-      selectedObject:elements[elements.indexOf(this.getElementsById(elements, element.id))],
-      indexSelectedObject: elements.indexOf(this.getElementsById(elements, element.id))
-    })
+        selectedElementId: element.id,
+        selectedObject: elements[elements.indexOf(this.getElementsById(elements, element.id))],
+        indexSelectedObject: elements.indexOf(this.getElementsById(elements, element.id)),
+      });
+    }
   }
 
-}
+
+  removeById(elements, id) {
+    const elementsUpdated = elements.filter(element => element.id !== id);
+    return elementsUpdated;
+  }
+
+  selectTool(selectedTools) {
+    const { selectedTool } = this.state;
+    if (selectedTools === '') {
+      this.setState({
+        selectedTool: '',
+        selectedElementId: '',
+      });
+    } else if (selectedTool === selectedTools) {
+      this.setState({
+        selectedTool: '',
+      });
+    } else {
+      this.setState({
+        selectedTool: selectedTools,
+        selectedElementId: '',
+      });
+    }
+  }
 
 
-deleteElement() {
+  updateElements(obj) {
+    const { elements } = this.state;
+    const newElements = Object.assign([], elements);
+    newElements.push(obj);
     this.setState({
-      elements: this.removeById(this.state.elements, this.state.selectedElementId),
-      selectedElementId:"",
-      selectedObject: {}
-    })
-}
+      elements: newElements,
+    });
+  }
 
+
+  /* o sa fie onClick la properties */
+  updateObjectInElements(obj) {
+    const { indexSelectedObject, elements } = this.state;
+    const newElements = Object.assign([], elements);
+    newElements[indexSelectedObject] = obj;
+    this.setState({
+      elements: newElements,
+    });
+  }
+
+  deleteElement() {
+    const { elements, selectedElementId } = this.state;
+    this.setState({
+      elements: this.removeById(elements, selectedElementId),
+      selectedElementId: '',
+      selectedObject: {},
+    });
+  }
 
 
   render() {
-    const { elements, selectedTool, selectedObject, selectedElementId, currentKeyArray, indexSelectedObject } = this.state;
+    const {
+      elements, selectedTool, currentKeyArray, indexSelectedObject,
+    } = this.state;
+    const {
+      selectedObject, selectedElementId,
+    } = this.state;
     return (
       <div className="App" id="App">
         <div className="header">
@@ -126,7 +128,7 @@ deleteElement() {
         </div>
         <div className="sidebar">
           <Sidebar
-            selectTool={this.selectTool.bind(this)}
+            selectTool={this.selectTool}
             selectedTool={selectedTool}
           />
         </div>
@@ -134,39 +136,37 @@ deleteElement() {
           <Drawer
             elements={elements}
             selectedTool={selectedTool}
-            setSelectedId={this.setSelectedId.bind(this)}
-            selectedTool={selectedTool}
-            updateElements={this.updateElements.bind(this)}
+            setSelectedId={this.setSelectedId}
+            updateElements={this.updateElements}
           />
-      </div>
-      <div className="mainComponent">
+        </div>
+        <div className="mainComponent">
           <MainComponent
-
-            deleteElement={this.deleteElement.bind(this)}
+            deleteElement={this.deleteElement}
             elements={elements}
-            setSelectedId={this.setSelectedId.bind(this)}
+            setSelectedId={this.setSelectedId}
             activateInput={this.activateInput}
             appState={this.state}
             selectedTool={selectedTool}
             selectedElementId={selectedElementId}
             currentKeyArray={currentKeyArray}
-            />
-      </div>
-      <div className="properties">
+          />
+        </div>
+        <div className="properties">
           <Properties
-            getValidationState={this.getValidationState.bind(this)}
+            getValidationState={this.getValidationState}
             elements={elements}
             selectedElementId={selectedElementId}
-            setSelectedId={this.setSelectedId.bind(this)}
+            setSelectedId={this.setSelectedId}
             indexSelectedObject={indexSelectedObject}
             appState={this.state}
-            updateObjectInElements={this.updateObjectInElements.bind(this)}
+            updateObjectInElements={this.updateObjectInElements}
             selectedObject={selectedObject}
             selectedTool={selectedTool}
           />
+        </div>
       </div>
-    </div>
-    )
+    );
   }
 }
 
--- src/Components/Main.jsx
+++ src/Components/Main.jsx
@@ -1,36 +1,33 @@
 import React, { Component } from 'react';
-import { Navbar, Nav, NavItem, NavDropdown, MenuItem, ButtonToolbar, Button, Glyphicon, ListGroup, ListGroupItem, ButtonGroup } from 'react-bootstrap';
-import Point from '../Models/Point.js';
-import Rectangle from '../Models/Rectangle.js';
-import Line from '../Models/Line.js';
-
+import {
+  ButtonToolbar, Button, Glyphicon,
+} from 'react-bootstrap';
 
 
 class MainComponent extends Component {
-
-render(){
-
-  const { elements, addElement,updatedValue, appState, deleteElement, setSelectedId, activateInput, addToInput, selectedTool, selectedElementId, modifyElement, changeUpdatedValue, currentKeyArray, currentKey } = this.props;
-  return(
-            <div className="col-sm-7">
-              <div className="top">
-                <ButtonToolbar className="addordeletebuttons">
-                  <Button
-                    className={'btn btn-danger' + (selectedElementId ? '' : ' disabled ')}
-                    onClick={() => deleteElement()}
-                  >
-                    <Glyphicon glyph="remove" />
-                  </Button>
-                </ButtonToolbar>
-              </div>
-
-              <div className="stringify">
-                {JSON.stringify(appState)}
-              </div>
-            </div>
-          )
-        }
-      }
+  render() {
+    const {
+      appState, deleteElement, selectedElementId,
+    } = this.props;
+    return (
+      <div className="col-sm-7">
+        <div className="top">
+          <ButtonToolbar className="addordeletebuttons">
+            <Button
+              className={`btn btn-danger${selectedElementId ? '' : ' disabled '}`}
+              onClick={() => deleteElement()}
+            >
+              <Glyphicon glyph="remove" />
+            </Button>
+          </ButtonToolbar>
+        </div>
+        <div className="stringify">
+          {JSON.stringify(appState)}
+        </div>
+      </div>
+    );
+  }
+}
 
 
 export default MainComponent;
